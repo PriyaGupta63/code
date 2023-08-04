4b50392dbf6e136e8accb46da106982bd0615e26
priya63940 <priyaguptacoder@gmail.com>
2023-08-04 14:13:31 +0530

    calculator

---
+++ Alpha-1/calculator.py
@@ -0,0 +1,12 @@
+num1 = int(input("enter the num1 here:"))
+opt = input("enter the operater here")
+num2 = int(input("enter the num2 here "))
+
+if opt == "+":
+    print(num1 + num2)
+elif opt == "-":
+    print(num1 - num2)
+elif opt == "*":
+    print(num1 * num2)
+elif opt == "/":
+    print(num1 / num2)
