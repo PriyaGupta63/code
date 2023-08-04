7b8f0ef060aed57e9696f3c94133b57db923054c
priya63940 <priyaguptacoder@gmail.com>
2023-08-04 14:18:13 +0530

    Grade calculator

---
+++ Alpha-1/Grade.py
@@ -0,0 +1,23 @@
+marks = int(input("enter your marks here"))
+
+
+if marks >= 90:
+    print("congrats your got A++ grade")
+elif marks >= 80:
+    print("congrats your got A+ grade")
+elif marks >= 70:
+    print("you got it B+ grade")
+elif marks >= 60:
+    print("you got it B grade")
+elif marks >= 50:
+    print("you got it C+ grade")
+elif marks >= 40:
+    print("you got it C grade")
+elif marks >= 30:
+    print("you got it D+ grade") 
+elif marks >= 20:
+    print("you got it D grade ")
+elif marks >= 10:
+    print("oops you are not qualified :()")
+else:
+    print("invalid input")    
