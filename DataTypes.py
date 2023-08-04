f15f381e5c6e1386d36216b58a66b99e0cb188a1
priya63940 <priyaguptacoder@gmail.com>
2023-08-04 14:33:00 +0530

    datatypes

---
+++ DataTypes.py
@@ -0,0 +1,41 @@
+#DataTypes in python
+
+#String:- 
+name = 'Priya'
+print(name)
+print(type(name)) #This is example of string
+
+#Integer:- 
+age = 21
+print(age)
+print(type(age)) #This is example of Integer
+
+#Boolean
+hi = True
+print(hi)
+print(type(hi)) #This is example of Boolean
+
+#Float
+num = 3.56
+print(num)
+print(type(num)) #This is example of Float
+
+#List
+Fruits = ['apple', 'mango', 'lichi', 'pine-apple', 'grape', 'orange']
+print(Fruits)
+print(type(Fruits))
+
+#Tuple
+Vegetable = ('tomato', 'potato', 'capcicum', 'garlic', 'onion', 'lady-finger')
+print(Vegetable)
+print(type(Vegetable))
+
+#Dictionary
+data = {
+    'name': 'Priya',
+    'age': 21,
+    'city': 'Lucknow',
+    'Course': 'BCA'     
+}
+print(data)
+print(type(data))
