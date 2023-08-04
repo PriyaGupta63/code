c60085aed7ae6b9c03fdce5ed6af77bac111835b
priya63940 <priyaguptacoder@gmail.com>
2023-08-04 14:36:57 +0530

    weather app

---
+++ weather.py
@@ -0,0 +1,10 @@
+current_weather = 'cloudy'
+
+if current_weather == 'rainy':
+    print('Bring raincoat')
+elif current_weather == 'sunny':
+    print('Grab sunglasses')
+elif current_weather == 'cloudy':
+    print('May be its rain today...take umbrella')
+elif current_weather == 'windy':
+    print('Thunder strom outside')
