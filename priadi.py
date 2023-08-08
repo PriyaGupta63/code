1fbdcac41afb176a24b66ae5a74f1151a9d2af96
priya63940 <priyaguptacoder@gmail.com>
2023-08-08 15:29:15 +0530

    Updated

---
+++ priadi.py
@@ -0,0 +1,12 @@
+weather = 'cloudy'
+
+if weather == 'sunny':
+    print('grab glasses')
+elif weather == 'rainy':
+    print('rainn coat') 
+elif weather == 'cloudy':
+    print('take umbrella') 
+else :
+    print('none')       
+
+
