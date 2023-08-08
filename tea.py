6b41f45f13ba5483c976ad049527975ad1aaed43
priya63940 <priyaguptacoder@gmail.com>
2023-08-08 15:29:41 +0530

    Updated

---
+++ tea.py
@@ -0,0 +1,79 @@
+# # tea = 'hard tea'
+
+# # if tea == 'black tea':
+# #     print('without milk')
+# # elif tea == 'milk tea':
+# #     print('without sugar')
+# # elif tea == 'hard tea':
+# #     print('with milk')
+# # else :
+# #     print('invalid input')   
+
+
+# # num1 = int(input('entet num1 here'))
+# # opt = input('enter opt here')
+# # num2 = int(input('enter num2 here'))
+
+
+# # if opt == '+':
+# #     print(num1 + num2)
+# # elif opt == '-':
+# #     print(num1 - num2)
+# # elif opt == '*':
+# #     print(num1 * num2)
+# # elif opt == '/':
+# #     print(num1 / num2)
+# # elif opt == '%':
+# #     print(num1 % num2)        
+# # else :
+# #     print('invalid input')
+
+
+# a = int(input("Enter a number: "))
+# # a%2 == 0
+
+# # % = Remainder Value
+# # / = Quotient Value
+# if (a%2) == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+
+
+# b = int(input('enter a number'))
+# #a%3 == 0
+# # % = remainder value 
+# # / quotient value 
+
+
+# if (b%2) != 0:
+#     print ("odd")
+# else: 
+#     print("even")
+    
+
+
+# c = int(input('enter a sum of num here'))
+
+  
+# a = list(range(40))
+# print(a)
+# print(a*6)
+
+
+# for x in range(10):
+#     print(2*x)
+#     print(2*x, end= (" , ") )
+
+# a = "priya, adi, tanu, abhay"    
+# for name in a:
+#     print(name*3) 
+
+
+# n = 6   
+# while n>= 0:
+#     print(n)
+#     n-=(1)
+    
---
+++ variablies.py
@@ -0,0 +1,34 @@
+name = 'priya gupta'
+print(name, type (name))
+age = 21
+print(age, type (age))
+b = True
+print(b, type (b))
+f = 2.8
+print(f, type (f))
+c = 1j
+print(c, type (c))
+l = ['apple, mango, banana, pineapple, watermillonb']
+print(l, type (l))
+n = None
+print(n, type (n))
+
+
+
+a = 32
+b = 43
+print(a + b)
+print(a - b)
+print(a * b)
+print(a ** b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+print(a > b )
+print(a < b )
+print(a == b)
+prb and )
+
+
+
